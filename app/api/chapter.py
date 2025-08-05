d480168c5a664664518e5817d5c187f44872ef36
Truc Tran <tructran@Trucs-MacBook-Pro.local>
2025-08-05 14:03:57 +0700

    Update code

--- app/api/chapter.py
+++ app/api/chapter.py
@@ -20,25 +20,25 @@ def get_db():
 def read_all(comic_id: int, db: Session = Depends(get_db)):
     return chapter.get_chapters(db, comic_id)
 
-@router.post("/chapters/crawl/")
-def create(db: Session = Depends(get_db)):
-    url = "https://dilib.vn/tham-tu-lung-danh-conan-14786.html"
+# @router.post("/chapters/crawl/")
+# def create(db: Session = Depends(get_db)):
+#     url = "https://dilib.vn/tham-tu-lung-danh-conan-14786.html"
 
-    # Gửi request lấy HTML
-    res = requests.get(url)
-    res.raise_for_status()
+#     # Gửi request lấy HTML
+#     res = requests.get(url)
+#     res.raise_for_status()
 
-    # Parse HTML
-    soup = BeautifulSoup(res.text, "html.parser")
+#     # Parse HTML
+#     soup = BeautifulSoup(res.text, "html.parser")
 
-    a_tags = soup.select("div.container > div.row div#primary fieldset > div.row > div > a")
-    print(len(a_tags))
+#     a_tags = soup.select("div.container > div.row div#primary fieldset > div.row > div > a")
+#     print(len(a_tags))
 
 
-    for a in a_tags:
-        print("zzz " + a.get_text(strip=True))
-        chapterCreate = ChapterCreate(title=a.get_text(strip=True), link="https://dilib.vn" + a.get("href"), comic_id=1)
-        chapter.create_chapter(db, chapterCreate)
-        print("Created chapter: ", a.get_text(strip=True))
+#     for a in a_tags:
+#         print("zzz " + a.get_text(strip=True))
+#         chapterCreate = ChapterCreate(title=a.get_text(strip=True), link="https://dilib.vn" + a.get("href"), comic_id=1)
+#         chapter.create_chapter(db, chapterCreate)
+#         print("Created chapter: ", a.get_text(strip=True))
 
-    return {"message": "Crawled chapters successfully"}
+#     return {"message": "Crawled chapters successfully"}
--- app/api/chapter_content.py
+++ app/api/chapter_content.py
@@ -21,24 +21,24 @@ def read_all(chapter_id: int, db: Session = Depends(get_db)):
     return chapter_content.get_chapters_content(db, chapter_id)
 
 
-@router.post("/chaptersContent/crawl/")
-def create(db: Session = Depends(get_db)):
-    all_chapters = chapter.get_chapters(db, 1)
-    for chap in all_chapters:
-        url = chap.link
-        res = requests.get(url)
-        res.raise_for_status()
-        soup = BeautifulSoup(res.text, "html.parser")
-        img_tags = soup.select("div.container > div.row div#primary img[alt*=\"Truyện Tranh Thám Tử Lừng Danh Conan\"]")
-        print(len(img_tags))
-        jpg_links = []
-        for img in img_tags:
-            src = img.get("src")
-            print("https://dilib.vn" + src)
-            print(len(img_tags))
-            chapter_content_create = ChapterContentCreate(chapter_id=chap.id, content="https://dilib.vn" + src)
-            chapter_content.create_chapter_content(db, chapter_content_create)
+# @router.post("/chaptersContent/crawl/")
+# def create(db: Session = Depends(get_db)):
+#     all_chapters = chapter.get_chapters(db, 1)
+#     for chap in all_chapters:
+#         url = chap.link
+#         res = requests.get(url)
+#         res.raise_for_status()
+#         soup = BeautifulSoup(res.text, "html.parser")
+#         img_tags = soup.select("div.container > div.row div#primary img[alt*=\"Truyện Tranh Thám Tử Lừng Danh Conan\"]")
+#         print(len(img_tags))
+#         jpg_links = []
+#         for img in img_tags:
+#             src = img.get("src")
+#             print("https://dilib.vn" + src)
+#             print(len(img_tags))
+#             chapter_content_create = ChapterContentCreate(chapter_id=chap.id, content="https://dilib.vn" + src)
+#             chapter_content.create_chapter_content(db, chapter_content_create)
 
-        print("Crawl " + chap.title + " done")
+#         print("Crawl " + chap.title + " done")
 
-    return {"message": "Crawled chapter content successfully"}
+#     return {"message": "Crawled chapter content successfully"}
